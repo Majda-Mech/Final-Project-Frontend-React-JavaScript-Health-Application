ca20f5816281690616a9e2a4db6972d26c910f57
Mechda <mechda.zariouh@gmail.com>
2023-03-16 12:24:22 +0100

    eindopdr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import NavBar from "./components/navigation/NavBar";
+import Home from "./pages/Home";
+import Recipe from "./pages/Recipe";
+import AboutUs from "./pages/AboutUs";
+import OurApp from "./pages/OurApp";
+import Contacts from "./pages/Contacts";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Footer from "../../../../WebstormProjects/frontend-eind-opdracht-calorie-health-app/src/components/footer/Footer";
+import {AuthContext} from "./components/context/AuthContext";
+import {useContext} from "react";
 
 function App() {
+
+  const {isAuth} =useContext(AuthContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+  <NavBar/>
+  <div className="content">
+    <Switch>
+      <Route exact path="/">
+      <Home/>
+      </Route>
+      <Route path="/recipe-info/:id">
+        {isAuth? <Recipe/> : <Redirect to="/log-in"/>}
+      </Route>
+      <Route path="/about-us">
+        <AboutUs/>
+      </Route>
+      <Route path="/our-app">
+        <OurApp/>
+      </Route>
+      <Route path="/contacts">
+        <Contacts/>
+      </Route>
+      <Route path="/log-in">
+        <LogIn/>
+      </Route>
+      <Route path="/sign-up">
+        <SignUp/>
+      </Route>
+    </Switch>
+</div>
+  <Footer/>
+</>
   );
 }
 
