f1ff49f6367f04ef5c8a26a90015dbb9f600d624
Majda Zariouh Zariouh <112877888+Majda-Mech@users.noreply.github.com>
2023-08-31 11:24:48 +0200

    Update NavBar.js

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -23,22 +23,16 @@ const NavBar = () => {
                 <img src={logo} alt="logo" className="logo-styling"/>
             </span>
             <span className="nav-center-styling">
-            <NavLink to="/" exact={true} activeClassName="active-link"> Home </NavLink>
-
-            <NavLink to="/about-us" activeClassName="active-link"> About Us </NavLink>
-
-            <NavLink to="/our-app" activeClassName="active-link"> Our App </NavLink>
-
-            <NavLink to="/contacts" activeClassName="active-link"> Contacts </NavLink>
+            <NavLink to="/" exact="true" activeclassname="active-link"> Home </NavLink>
+            <NavLink to="/about-us" activeclassnamee="active-link"> About Us </NavLink>
+            <NavLink to="/our-app" activeclassname="active-link"> Our App </NavLink>
+            <NavLink to="/contacts" activeclassame="active-link"> Contacts </NavLink>
             </span>
-
-            <span className="nav-right-styling"> Welcome {user && user.username}
-
+            <span className="nav-right-styling">
+                Welcome {user && user.username}
             { !isAuth?
-                <NavLink to="/log-in" activeClassName="active-link">
-                    Log in </NavLink> : <Button clickHandler={logOutFunction}>Logout</Button>
-            }
-
+                <NavLink to="/log-in" activeclassname="active-link">
+                    Log in </NavLink> : <Button clickHandler={logOutFunction}>Logout</Button> }
                 {!isAuth?
                 <Button clickHandler={signUpFunction} >Sign Up</Button> : ""}
             </span>
